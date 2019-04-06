d083ee8e177406363dc77233ef46a78a5a898300
szigetiatti <szigeti.attila96@gmail.com>
2019-04-06 21:22:21 +0200

    Bootstrap és Font awesome használata

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="webshop.css">
     <title>Webshop</title>
 </head>
 <body>
-
-    <h1>Termékek</h1>
-
+    <?php include 'navbar.html'; ?>
+    <div class="container main-content">
+        <div class="jumbotron">
+            <h1>Kezdőlap</h1>
+            <p class="lead">Termékek adatbázis, vásárlások adminisztrációja</p>
+        </div>
+    </div>
 </body>
 </html>
