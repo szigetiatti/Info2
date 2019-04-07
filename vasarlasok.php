7f9feb51ac514a7b74eb50ab4c8a0bbad727b74b
szigetiatti <szigeti.attila96@gmail.com>
2019-04-07 19:25:03 +0200

    TODO

---
+++ vasarlasok.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="webshop.css">
+    <title>Webshop</title>
+</head>
+<body>
+    <?php include 'navbar.html'; ?>
+    <div class="container main-content">
+        <div class="jumbotron ">
+            <h1>Vásárlások</h1>
+
+        </div>
+    </div>
+    
+  </body>
+</body>
+</html>
